3516c0f684253dc2b4514469e7178588dab54caa
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-03-20 11:54:00 +0530

    - Updated CSS for FarmerApplyForm to improve styling, including responsive adjustments and custom file input styling.

--- Frontend/src/components/Farmer.jsx
+++ Frontend/src/components/Farmer.jsx
@@ -4,6 +4,7 @@ import Dashboard from '../components/Dashboard/Dashboard';
 import Sidebar from './Sidebar/Sidebar';
 import { FaBars, FaSeedling } from 'react-icons/fa';
 import '../components/Dashboard/Dashboard.css';
+import ProfileFarmer from './ProfileFarmer/ProfileFarmer';
 
 const Farmer = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -33,6 +34,7 @@ const Farmer = () => {
         <Routes>
           <Route path="/" element={<Navigate to="/farmer/dashboard" replace />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<ProfileFarmer />} />
           
           {/* Add more routes here as needed */}
         </Routes>
